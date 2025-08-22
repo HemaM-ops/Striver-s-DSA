2f5db67556736fe8bbf417d15cd5197e577cd409
M Hemasri <mahadevamangalamhemasri@gmail.com>
2025-08-22 19:02:52 +0530

    Print name  N times using Recursions

--- Lecture 5 - Learn Basic Recursion/PrintName.java
+++ Lecture 5 - Learn Basic Recursion/PrintName.java
@@ -3,18 +3,25 @@
 
 // PRINT NAME 5 TIMES USING RECURSION
 
+import java.util.Scanner;
+
 public class PrintName{
     public static int count=0;
 
-    public void printName(){
-        if(count==5) return;
+    public void printName(int i,int n){
+        if(i>n) return;
         System.out.println("Hemasri");
         count++;
-        printName();
+        printName(i+1,n);
     }
 
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         PrintName p = new PrintName();
-        p.printName();
+        System.out.println("Type the number of times you want you print your name: ");
+        int n = sc.nextInt();
+
+        p.printName(1,n);
+        sc.close();
     }
 }
