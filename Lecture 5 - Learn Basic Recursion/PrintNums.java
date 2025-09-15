30170fa11e9f3d0ec0eecd55289a96f8adc4ca99
M Hemasri <mahadevamangalamhemasri@gmail.com>
2025-09-15 17:06:43 +0530

    Print 1 to N linearly using recursions

--- Lecture 5 - Learn Basic Recursion/PrintNums.java
+++ Lecture 5 - Learn Basic Recursion/PrintNums.java
@@ -2,12 +2,12 @@
 // 15.09.25 - Monday
 // print numbers from n to 1 using recursion linearly
 public class PrintNums{
-    public static void printNums(int n){
-        if(n==0) return;
-        System.out.println(n);
-        printNums(n-1);
+    public static void printNums(int i,int n){
+        if(i>n) return;
+        System.out.println(i);
+        printNums(i+1,n);
     }
     public static void main(String[] args){
-        printNums(5);
+        printNums(1,5);
     }
 }
