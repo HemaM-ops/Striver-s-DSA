b2728f651e5c12d19980751eb9577eb9f99a1a92
M Hemasri <mahadevamangalamhemasri@gmail.com>
2025-09-16 08:31:32 +0530

    Factorial of a number

---
+++ Lecture 5 - Learn Basic Recursion/Factorial.java
@@ -0,0 +1,15 @@
+// M Hemasri - AIE22028
+// 16.09.25 - Tuesday
+
+// print factorial of n - functional Recursion
+
+public class Factorial{
+    public static int fact(int n){
+        if(n==0) return 1;
+        return n*fact(n-1);
+    }
+
+    public static void main(String[] args){
+        System.out.println(fact(5));
+    }
+}
